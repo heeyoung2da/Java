2915456e6a0ec2b75992196fb8fa3f522ddad819
김희영 <mnkl1225@gmail.com>
2021-07-21 21:20:35 +0900

    delete file

--- Algo/src/Baek_10951.java
+++ Algo/src/Baek_10951.java
@@ -1,20 +1,22 @@
+// 테스트 케이스 개수가 정해지지 않은 경우
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Baek_10951 {
-	public static void main(String args[]) throws IOException {
+	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		String str;
 
-		while( (str=br.readLine()) != null ){
-
-			int a = str.charAt(0) - 48;
-			int b = str.charAt(2) - 48;
+		while((str=br.readLine()) != null ){
+			StringTokenizer st = new StringTokenizer(str);
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
 			sb.append(a+b).append("\n");
-
 		}
 		System.out.print(sb);
 	}
---
+++ Algo/src/Baek_10952.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baek_10952 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		String str;
+
+		while((str=br.readLine()) != null ){
+			StringTokenizer st = new StringTokenizer(str);
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+
+			if(a==0 && b==0){
+				break;
+			}
+			sb.append(a+b).append("\n");
+		}
+		System.out.print(sb);
+	}
+}
