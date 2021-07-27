99aba280ae10c15abaf48f84e0338bb0394460a7
김희영 <mnkl1225@gmail.com>
2021-07-27 22:43:40 +0900

    add Baekjoon problems-io

---
+++ Algo/src/com/hy/algo/io/Baek_11718.java
@@ -0,0 +1,13 @@
+package com.hy.algo.io;
+
+import java.util.Scanner;
+
+public class Baek_11718 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while(sc.hasNextLine()){
+			String str = sc.nextLine();
+			System.out.println(str);
+		}
+	}
+}
