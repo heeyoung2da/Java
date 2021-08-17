24a900bfa14a7ce05a57e689de8154231a9b31be
김희영 <mnkl1225@gmail.com>
2021-08-18 00:19:57 +0900

    Add BOJ Problem

---
+++ Algo/src/com/algo/baek/Main17413.java
@@ -0,0 +1,50 @@
+package com.algo.baek;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+// 단어 뒤집기2
+public class Main17413 {
+	static StringBuilder sb;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+		Stack<Character> stack = new Stack<Character>();
+		boolean markChk = false;
+
+		String str = br.readLine();
+		for (int i = 0; i < str.length(); i++) {
+			char c = str.charAt(i);
+
+			// < >
+			if (c == '<') {
+				popFunc(stack);
+				markChk = true;
+				sb.append(c);
+			} else if (c == '>') {
+				markChk = false;
+				sb.append(c);
+			} else if (markChk) {
+				sb.append(c);
+			} else {
+				if (c == ' ') {
+					popFunc(stack);
+					sb.append(c);
+				} else {
+					// 단어만
+					stack.push(c);
+				}
+			}
+		}
+		popFunc(stack);
+		System.out.print(sb);
+	}
+	private static void popFunc(Stack<Character> stack){
+		while (!stack.isEmpty()) {
+			sb.append(stack.pop());
+		}
+	}
+}
+
