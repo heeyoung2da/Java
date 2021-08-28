5b64e965b744154830c1b8b5865af5e24f10e782
김희영 <mnkl1225@gmail.com>
2021-08-28 23:02:21 +0900

    Add BOJ Problem

---
+++ Algo/src/com/algo/baek/implementation/Main2669.java
@@ -0,0 +1,28 @@
+package com.algo.baek.implementation;
+
+import java.util.Scanner;
+
+// 직사각형 네개의 합집합의 면적 구하기
+public class Main2669 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int area = 0;
+		int[][] map = new int[101][101];
+		for(int t=0; t<4; t++){
+			int leftX = sc.nextInt();
+			int leftY = sc.nextInt();
+			int rightX = sc.nextInt();
+			int rightY = sc.nextInt();
+
+			for(int x=leftX; x<rightX; x++){
+				for(int y=leftY; y<rightY; y++){
+					if(map[x][y]!=1){
+						map[x][y] = 1;
+						area++;
+					}
+				}
+			}
+		}
+		System.out.println(area);
+	}
+}
