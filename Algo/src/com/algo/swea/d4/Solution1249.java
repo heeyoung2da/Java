522e7272e291a15bb5e3b15fd151fadb9dadfbbe
김희영 <mnkl1225@gmail.com>
2021-10-13 09:38:24 +0900

    Update Files

--- Algo/src/com/algo/swea/d4/Solution1249.java
+++ Algo/src/com/algo/swea/d4/Solution1249.java
@@ -7,6 +7,7 @@ import java.util.StringTokenizer;
 
 // 보급로
 public class Solution1249 {
+	private static final int INF = Integer.MAX_VALUE;
 	static int N;
 	static int[][] map, visited;
 	static int[] dr = {-1, 1, 0, 0}; // 상하좌우
@@ -21,30 +22,61 @@ public class Solution1249 {
 			N = Integer.parseInt(br.readLine());
 			map = new int[N][N];
 
-			st = new StringTokenizer(br.readLine());
 			for(int i=0; i<N; i++){
+				char[] ch = br.readLine().toCharArray();
 				for(int j=0; j<N; j++){
-					map[i][j] = Integer.parseInt(st.nextToken());
+					map[i][j] = ch[j] - '0';
 				}
 			}
 
-			move(0,0);
+			System.out.println("#"+t+" "+dijkstra(0,0));
+
 
 		}
 	}
-	private static void move(int r, int c){
-		int nr = r, nc = c, cnt=0;
-		visited = new int[N][N];
 
-		for(int d=0; d<4; d++){
-			nr += dr[d];
-			nc += dc[d];
+	private static int dijkstra(int startR, int startC) {
+		boolean[][] visited = new boolean[N][N];
+		int[][] minTime = new int[N][N];
 
-			if(nr<0 || nr>=N || nc<0 || nc>=N || visited[nr][nc] ==1) continue;
+		// 최소값이 갱신되도록 큰 값으로 초기화
+		for(int i=0; i<N; i++){
+			for(int j=0; j<N; j++){
+				minTime[i][j] = INF;
+			}
+		}
+		minTime[startR][startC] = 0;
 
-			visited[nr][nc] = 1;
-			cnt++;
+		int r=0, c=0, minCost=0, nr, nc;
+		while(true){
+			// step1
+			minCost = INF;
+			for(int i=0; i<N; i++){
+				for(int j=0; j<N; j++){
+					if(!visited[i][j] && minCost > minTime[i][j]){
+						minCost = minTime[i][j];
+						r=i;
+						c=j;
+					}
+				}
+			}
+			visited[r][c] = true;
+			if(r==N-1 && c==N-1) return minCost;
+
+			// step2 : step1에서 선택된 정점을 경유지로 해서 인접정점 따져보기
+			// 이 문제에서는 인접정점이 4방 정점
+			for(int d=0; d<4; d++){
+				nr = r + dr[d];
+				nc = c + dc[d];
+
+				if(nr>=0 && nr<N && nc>=0 && nc<N && !visited[nr][nc]
+						&&minTime[nr][nc] > minCost + map[nr][nc]){
+					minTime[nr][nc] = minCost + map[nr][nc];
+				}
+			}
+
 
 		}
 	}
+
 }
