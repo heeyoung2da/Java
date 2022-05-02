8230e10f4c582d400f6d5afe87b69d43460470ba
heeyoung2da <mnkl1225@gmail.com>
2022-05-02 22:05:29 +0900

    Add BOJ Problem

---
+++ Algo/src/com/algo/baek/Main2501.java
@@ -0,0 +1,26 @@
+package com.algo.baek;
+
+import java.util.Scanner;
+
+// 약수 구하기
+public class Main2501 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+
+		for(int i=1; i<=N; i++){
+			if(N%i==0){
+				K--;
+			}
+
+			if(K==0){
+				System.out.println(i);
+				break;
+			}
+		}
+		if(K!=0){
+			System.out.println(0);
+		}
+	}
+}
